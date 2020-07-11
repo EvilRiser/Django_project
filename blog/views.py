6c200261322183b26da00f70b7319e259ba3cf51
SS <shaurabh.saha@gmail.com>
2020-07-12 00:56:50 +0530

    creating templates and extending class

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,35 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+
+posts = [
+	{
+		'author': 'CoreyMs',
+		'title': 'Blog Post 1',
+		'content': 'First Post content',
+		'date_posted': 'July 11,2020'
+	},
+	{
+		'author': 'Jane Doe',
+		'title': 'Blog Post 2',
+		'content': 'Second Post content',
+		'date_posted': 'July 12,2020'
+	}
+
+
+
+]
+
+
 # Create your views here.
 def home(request):
-	return HttpResponse('<h1> Blog Home</h1>')
+	context = {
+		'posts': posts
+
+	}
+	# return HttpResponse('<h1> Blog Home</h1>')
+	return render(request,'blog/home.html',context)
 
 def about(request):
-	return HttpResponse('<h1> Blog About</h1>')
+	# return HttpResponse('<h1> Blog About</h1>')
+	return render(request,'blog/about.html',{'title':'About'})
